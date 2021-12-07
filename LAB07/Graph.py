e1ef0b5ceda8f2a7845f40e4a0e9b117415e35b9
vincenzo2100 <32100958+vincenzo2100@users.noreply.github.com>
2021-12-07 19:54:05 +0100

    LAB07

---
+++ LAB07/Graph.py
@@ -0,0 +1,99 @@
+from enum import Enum
+from typing import Any,Optional,Dict,List,Callable
+from Queue import Queue
+
+class EdgeType(Enum):
+    directed = 1
+    undirected = 0 
+        
+class Vertex:
+    data:Any
+    index: int
+
+    def __init__(self,data:Any,index: int) -> None:
+        self.data = data
+        self.index = index
+        
+class Edge:
+    source: Vertex
+    destination: Vertex
+    weight: Optional[float]
+
+class Graph:
+    adjacencies: Dict[Vertex,List[Edge]]
+
+    def __init__(self,adjacencies=None) -> None:
+        if(adjacencies == None):
+            adjacencies={}
+        self.adjacencies = adjacencies
+
+    def create_vertex(self,index:int, data: Any) -> Vertex:
+        vertex = Vertex(data,index)
+        self.adjacencies[str(index)+": "+str(data)]=[]
+
+    def add_directed_edge(self, source: Vertex, destination: Vertex, weight: Optional[float] = None) -> None:
+        self.adjacencies[source].append(destination)
+        if(weight is not None):
+            self.adjacencies[source].append(weight)
+    
+    def add_undirected_edge(self, source: Vertex, destination: Vertex, weight: Optional[float] = None) -> None:
+        self.adjacencies[source].append(destination)
+        self.adjacencies[destination].append(source)
+        if(weight is not None):
+            self.adjacencies[source].append(weight)
+            self.adjacencies[destination].append(weight)
+
+    def add(self, edge: EdgeType, source: Vertex, destination: Vertex, weight: Optional[float] = None) -> None:
+        if(edge==1):
+            self.add_directed_edge(source,destination,weight)
+        elif(edge==2):
+            self.add_undirected_edge(source,destination,weight)
+
+    def BTS(self,vertex: Vertex):
+        visited = []
+        queue = Queue()
+        visited.append(vertex)
+        queue.enqueue(vertex)
+        while queue:
+            v = queue.dequeue()
+            print(v,end=' ')
+            for neighbour in self.adjacencies[v]:
+                if neighbour not in visited:
+                    visited.append(neighbour)
+                    queue.enqueue(neighbour)
+
+    def dfs_util(self,v, visited):
+        visited.add(v)
+        print(v, end=' ')
+        for neighbour in self.adjacencies[v]:
+            if(neighbour not in visited):
+                self.dfs_util(neighbour,visited)
+
+    def DFS(self,v: Vertex):
+        visited = set()
+        self.dfs_util(v,visited)
+   
+    def show(self):
+        for key, value in self.adjacencies.items():
+            print(key, '---->', value)
+
+
+    def __generate_edges(self):
+        edges = []
+        for vertex in self.adjacencies:
+            for neighbour in self.adjacencies[vertex]:
+                if {neighbour, vertex} not in edges:
+                    edges.append({vertex, neighbour})
+        return edges
+
+    def __str__(self)->str:
+        res = "vertices: "
+        for k in self.adjacencies:
+            res += str(k) + " "
+        res += "\nedges: "
+        for edge in self.__generate_edges():
+            res += str(edge) + " \n"
+        return res
+        
+
+
